78df4f2b5116dfe452cbec6fc21be2997d68cd68
Diogo Paschoal <diogo.paschoal@protonmail.com>
2023-10-01 00:59:08 -0300

    Adding configuration to improve tests.

--- src/pages/SignUpPage.test.js
+++ src/pages/SignUpPage.test.js
@@ -1,9 +1,9 @@
-const SignUpPage = require("./SignUpPage.vue");
-const vtl = require("@testing-library/vue");
-const {render, screen} = vtl;
+import SignUpPage from "./SignUpPage.vue";
+import {render, screen} from "@testing-library/vue";
+import "@testing-library/jest-dom";
 
 it("has Sign Up header", () =>{
     render(SignUpPage);
     const header = screen.queryByRole("heading", { name: "Sign Up"});
-    expect(header).not.toBeNull();
+    expect(header).toBeInTheDocument();
 });
