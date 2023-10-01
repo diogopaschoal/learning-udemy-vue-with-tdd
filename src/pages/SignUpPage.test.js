427d9d41fa1b5bc2569bbaa43b0d07d54cce8618
Diogo Paschoal <diogo.paschoal@protonmail.com>
2023-10-01 00:29:50 -0300

    Tests running successfully

---
+++ src/pages/SignUpPage.test.js
@@ -0,0 +1,9 @@
+const SignUpPage = require("./SignUpPage.vue");
+const vtl = require("@testing-library/vue");
+const {render, screen} = vtl;
+
+it("has Sign Up header", () =>{
+    render(SignUpPage);
+    const header = screen.queryByRole("heading", { name: "Sign Up"});
+    expect(header).not.toBeNull();
+});
