bbf44ab2a6eca3493006aa6e801e2b8f4a5a805f
Diogo Paschoal <diogo.paschoal@protonmail.com>
2023-10-08 15:02:57 -0300

    Adding tests to username and e-mail.

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,3 +1,8 @@
 <template>
     <h1>Sign Up</h1>
+    <label for="username">Username</label>
+    <input id="username" placeholder="username" />
+    
+    <label for="email">E-mail</label>
+    <input id="email" placeholder="e-mail" />
 </template>
