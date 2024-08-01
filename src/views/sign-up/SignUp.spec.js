8812c3c6ee4c31bbfe44701d115fe4fc64ab9b32
Diogo Paschoal <diogo.paschoal@thoughtworks.com>
2024-08-01 10:40:37 -0300

    Section 2 - Sign Up Form

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -1,10 +1,55 @@
 import { render, screen } from "@testing-library/vue";
 import SignUp from "./SignUp.vue";
+import { expect } from "vitest";
 
 describe("Sign Up", () => {
   it("has Sign Up header", () => {
     render(SignUp);
     const header = screen.getByRole("heading", { name: "Sign Up" });
     expect(header).toBeInTheDocument();
+  });
+
+  it("has username input", () => {
+    render(SignUp);
+    expect(screen.queryByLabelText("Username")).toBeInTheDocument();
+  });
+
+  it("has email input", () => {
+    render(SignUp);
+    expect(screen.queryByLabelText("E-mail")).toBeInTheDocument();
+  });
+
+  it("has password input", () => {
+    render(SignUp);
+    expect(screen.queryByLabelText("Password")).toBeInTheDocument();
+  });
+
+  it("has password repeat input", () => {
+    render(SignUp);
+    expect(screen.queryByLabelText("Password Repeat")).toBeInTheDocument();
+  });
+
+  it("has password type for password input", () => {
+    render(SignUp);
+    expect(screen.queryByLabelText("Password")).toHaveAttribute(
+      "type",
+      "password"
+    );
+  });
+  it("has password type for password repeat input", () => {
+    render(SignUp);
+    expect(screen.queryByLabelText("Password Repeat")).toHaveAttribute(
+      "type",
+      "password"
+    );
+  });
+  it("has a Sign Up button", () => {
+    render(SignUp);
+    const button = screen.getByRole("button", { name: "Sign Up" });
+    expect(button).toBeInTheDocument();
+  });
+  it("disables the button initially", () => {
+    render(SignUp);
+    expect(screen.getByRole("button", { name: "Sign Up" })).toBeDisabled();
   });
 });
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Sign Up</h1>
+  <div>
+    <label for="username">Username</label>
+    <input id="username" />
+  </div>
+  <div><label for="email">E-mail</label>
+    <input id="email" />
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input id="password" type="password" />
+  </div>
+  <div>
+    <label for="passwordRepeat">Password Repeat</label>
+    <input id="passwordRepeat" type="password" />
+  </div>
+  <div>
+    <button disabled>Sign Up</button>
+  </div>
 </template>
